33d0aef8600e0062ac111c5f22a047ab3dfaf309
Luis Humberto Navarro <ribeiro.beton@gmail.com>
2024-01-28 22:44:39 -0400

    domingo

--- 03-counterapp-vite/src/CounterApp.jsx
+++ 03-counterapp-vite/src/CounterApp.jsx
@@ -1,17 +1,30 @@
 import PropTypes from 'prop-types';
+import {useState} from 'react';
+
 
 export const CounterApp = ({value}) => {
 
+    const[ counter,setCounter]=useState(value);
+
     const handleAdd=()=> {
-        console.log('+1');
-        value=1000;
-        console.log(value)
+        //setCounter(counter+1);
+        setCounter((c)=>c+1);
+    }
+    const menosUno=()=> {
+        //setCounter(counter+1);
+        setCounter((c)=>c-1);
+    }
+    const resetHandler=()=> {
+        //setCounter(counter+1);
+        setCounter(value);
     }
     return (  
         <>
             <h1>CounterApp</h1>
-            <h2> {value} </h2>
+            <h2> {counter} </h2>
             <button onClick={handleAdd}>+1</button>
+            <button onClick={menosUno}>-1</button>
+            <button onClick={resetHandler}>reset</button>
         </>
 
     );

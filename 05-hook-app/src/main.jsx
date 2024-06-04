b7845542b446b001381bf63ae3af89c9544b2d5f
Luis Humberto Navarro <ribeiro.beton@gmail.com>
2024-06-04 11:59:54 -0400

    un poco avance

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -10,12 +10,13 @@ import { Layout } from './05-useLayoutEffect/Layout'
 import { Memorize } from './06-memos/Memorize'
 import { MemoHook } from './06-memos/MemoHook'
 import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
 
 // import { HooksApp } from './HooksApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <CallbackHook/>
+    <Padre/>
     
   //</React.StrictMode>,
 )

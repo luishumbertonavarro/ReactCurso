67a9561a67b7063a1aecf67b8ca49cc89781d092
Luis Humberto Navarro <ribeiro.beton@gmail.com>
2024-07-19 15:52:47 -0400

    nueva seccion

--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Navigate, Route, Routes, Link } from 'react-router-dom'
+import { HomePage } from './HomePage';
+import { AboutPage } from './AboutPage';
+import { LoginPage } from './LoginPage';
+import { Navbar } from './Navbar';
 
 export const MainApp = () => {
     return (
         <>
             <h1>MainApp</h1>
-            <hr/>
+
+            {/* <Link to=''>Home</Link>
+            <Link to="/about">about</Link>
+            <Link to="/login">Login</Link> */}
+            <Navbar/>
+            <hr />
+
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='login' element={<LoginPage />} />
+                <Route path='about' element={<AboutPage />} />
+
+                {/* <Route path='/*' element={<LoginPage/>}/> */}
+
+                <Route path='/*' element={<Navigate to="/about" />}/>
+            </Routes>   
         </>
     )
 }
